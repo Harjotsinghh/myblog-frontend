66df9c15002bf4e30b12232efb8b27644ccc1e7f
Harjot Singh <harjot@Harjots-MacBook-Air.local>
2021-06-07 19:43:38 +0530

    changed frontend style and added signup fetch method

--- scripts/auth.js
+++ scripts/auth.js
@@ -20,3 +20,41 @@ loginform.addEventListener('submit',function(e){
 
 });
 
+
+
+$('#signup').submit(function(e){
+    e.preventDefault();
+   const fd= new FormData(this);
+   const entries= fd.entries();
+   const data= Object.fromEntries(entries);
+//    console.log(data);
+    fetch("http://localhost:8080/signup",{
+        method:"POST",
+        headers:{
+            "content-type" :"application/json"
+        },
+        body:JSON.stringify(data)
+    })
+    .then(res=>{
+        // console.log(res.json());
+        return res.json();
+    })
+    .then(res=>{
+        if(res.error)
+        {
+            $('#signup-error').html(res.error);
+        }
+        else{
+            $('#signup-error').html('Account Created succesfully');
+            console.log(res);
+            $('#signup')[0].reset();
+        }
+       
+    })
+    .catch(err=>{
+        $('#signup-error').html(err.message);
+        console.log(err);
+    })
+
+}
+)
--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,14 @@ var backdrop = document.querySelector(".backdrop");
 var ham= document.querySelector(".hamburger");
 ham.addEventListener('click',()=>{
     ham.classList.toggle("change");
-    backdrop.classList.toggle("toggle");
+       backdrop.classList.toggle("toggle")
+   
 })
 backdrop.addEventListener('click',()=>{
     ham.classList.toggle("change");
-    backdrop.classList.toggle("toggle");
+    backdrop.classList.toggle("toggle");    
+
+   
 })
 
 
@@ -19,11 +22,12 @@ fetch("http://localhost:8080/")
      throw new Error('error');
     return res.json()})
 .then(res=>{
-    res.posts.map(info=>{
+    res.posts.map((info ,ind)=>{
         var date=info.createdAt;
-        console.log(info);
+        // console.log(info);
         date=date.substring(0,10);
-        var ele= `<div class="post-title">
+        var ele= `
+        <a href=""><div class="post-title">
             <h1>${info.title}</h1>
             <div class="post-user">
                 <h3 class="name">Author: ${info.userId.username}</h3>
@@ -32,18 +36,25 @@ fetch("http://localhost:8080/")
         </div>
 
         <div class="post-info">
-            <p>${info.text}</p>
-            <a href=""><div class="fadeout"></div>READ MORE.......</a>
+            <p>${info.text} </p>
+            <div class="fadeout"></div>
         </div>
+        </a>
         
         `;
        
 
     var newele= document.createElement('div');
     newele.classList.add("post");
+    // console.log(ind);
+        if(ind%2==0)
+        newele.style["background-color"]= 'rgb(196, 224, 228)';
+        else
+        newele.style["bacground-color"]='rgba(86, 126, 63, 0.349)';
     newele.innerHTML=ele;
     posts.appendChild(newele);
 
+    
    
     });
     console.log(ans);
