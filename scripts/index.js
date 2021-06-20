52afa9cdf85db3b4e1b9b0f61ad909dfedfd3e6e
Harjot Singh <harjot@FC19LMAC771.local>
2021-06-20 23:25:37 +0530

    added the options in the hamburger menu which would be visible on clicking the hamburger icon

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 
 var backdrop = document.querySelector(".backdrop");
 var ham= document.querySelector(".hamburger");
+var content = document.querySelector(".backdrop-menu");
 ham.addEventListener('click',()=>{
     ham.classList.toggle("change");
        backdrop.classList.toggle("toggle")
+      content.classList.toggle("hide");
    
 })
 backdrop.addEventListener('click',()=>{
     ham.classList.toggle("change");
     backdrop.classList.toggle("toggle");    
-
+     content.classList.toggle("hide");
    
 })
 
